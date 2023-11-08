5b750f5492818261cb954157557aff45707321e9
Leeuw <mohamedessabir20@gmail.com>
2023-11-08 19:18:57 +0100

    First commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
 }
 
+
 android {
     namespace = "me.ensa.professeur"
     compileSdk = 34
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "Professeur"
 include(":app")
- 
+
